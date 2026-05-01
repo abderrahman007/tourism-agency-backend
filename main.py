ea19cf8e37d58a313b3e61c062950cb16d69ccab
abderrahman <abdoubenmalek344@gmail.com>
2026-05-01 17:49:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,6 @@ app.add_middleware(
 
 app.include_router(approuter)
 
-
-import uvicorn
-uvicorn.run("main:app", host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
